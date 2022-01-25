584859613f2ccd25e63523db1a3eb533204e9983
YizYah <yyakovson613@gmail.com>
2022-01-25 17:34:17 +0200

    feat(creategeneralinfo.ts): add static info to general
    should be possible to see static info in general
    
    fix #38

--- src/custom/fileGeneration/context/createGeneralInfo.ts
+++ src/custom/fileGeneration/context/createGeneralInfo.ts
@@ -13,6 +13,7 @@ export async function createGeneralInfo(nsInfo: NsInfo, codeDir: string) {
     general.json = codePackageJson
   }
 
+  general.static = nsInfo.static
   general.codeDir = codeDir
   return general
 }
---
+++ test/custom/createGeneralInfo.test.ts
@@ -0,0 +1,14 @@
+import test from 'ava'
+
+import { createGeneralInfo } from '../../src/custom/fileGeneration/context/createGeneralInfo'
+
+const codeDir = "foo"
+import { nsInfo } from './data/nsInfo'
+// import { generalInfo } from './data/generalInfo'
+
+
+test('createGeneralInfo', async t => {
+    const result = await createGeneralInfo(nsInfo, codeDir)
+    t.is(result.static.userType[1].slug, "1")
+
+})
